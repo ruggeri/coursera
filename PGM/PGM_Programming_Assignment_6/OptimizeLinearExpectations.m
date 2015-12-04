23acf0aadcf598efee8192af4ca6e17c9de2591e
Ned Ruggeri <ruggeri@self-loop.com>
2015-12-03 18:32:33 -0800

    finish PA6

--- PGM/PGM_Programming_Assignment_6/OptimizeLinearExpectations.m
+++ PGM/PGM_Programming_Assignment_6/OptimizeLinearExpectations.m
@@ -13,21 +13,45 @@ function [MEU OptimalDecisionRule] = OptimizeLinearExpectations( I )
   % This is similar to OptimizeMEU except that we will have to account for
   % multiple utility factors.  We will do this by calculating the expected
   % utility factors and combining them, then optimizing with respect to that
-  % combined expected utility factor.  
-  MEU = [];
-  OptimalDecisionRule = [];
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  %
-  % YOUR CODE HERE
-  %
-  % A decision rule for D assigns, for each joint assignment to D's parents, 
-  % probability 1 to the best option from the EUF for that joint assignment 
-  % to D's parents, and 0 otherwise.  Note that when D has no parents, it is
-  % a degenerate case we can handle separately for convenience.
-  %
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  
+  % combined expected utility factor.
+  D = I.DecisionFactors(1);
+  MEU = 0;
+  EUF = computeEUF(I);
+  OptimalDecisionRule = D;
 
+  indexToAssignmentsD = IndexToAssignment(1:length(D.val), D.card);
 
+  i = 1;
+  while i <= length(D.val)
+    range = i:(i+D.card(1)-1);
 
+    bestAssignment = i;
+    bestUtils = EUF.val(i);
+
+    for i2=range
+      assignment = indexToAssignmentsD(i2, :);
+      val = GetValueOfAssignment(EUF, assignment, D.var);
+      if bestUtils < val
+        bestAssignment = i2;
+        bestUtils = val;
+      end
+    end
+
+    OptimalDecisionRule.val(range) = 0;
+    OptimalDecisionRule.val(bestAssignment) = 1;
+    MEU += bestUtils;
+
+    i += D.card(1);
+  end
+end
+
+function EUF = computeEUF(I)
+  EUF = struct("var", [], "card", [], "val", []);
+
+  for utilityIdx=1:length(I.UtilityFactors)
+    I2 = struct("RandomFactors", I.RandomFactors,
+                "DecisionFactors", I.DecisionFactors,
+                "UtilityFactors", I.UtilityFactors(utilityIdx));
+    EUF = FactorSum(EUF, CalculateExpectedUtilityFactor(I2));
+  end
 end
