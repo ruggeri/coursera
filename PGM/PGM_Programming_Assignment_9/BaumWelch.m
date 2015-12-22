fbbc5844ec159cdbecb16b045ce411b3214a6fb8
Ned Ruggeri <ruggeri@self-loop.com>
2015-12-22 10:17:28 -0800

    PGM PA9: fixes to Baum-Welch
    Still not working...

--- PGM/PGM_Programming_Assignment_9/BaumWelch.m
+++ PGM/PGM_Programming_Assignment_9/BaumWelch.m
@@ -1,11 +1,13 @@
 function [ClassProb PairProb] = BaumWelch(P, actionData, poseData, logEmissionProbs)
-  NUM_ACTIONS = size(actionData, 1);
+  NUM_ACTIONS = length(actionData);
   NUM_CLASSES = length(P.c);
   NUM_POSES = size(poseData, 1);
   NUM_TRANSITIONS = NUM_POSES - NUM_ACTIONS;
 
   ClassProb = zeros(NUM_POSES, NUM_CLASSES);
   PairProb = zeros(NUM_TRANSITIONS, NUM_CLASSES ^ 2);
+
+  logTransMatrix = log(P.transMatrix);
 
   for actionIdx=1:NUM_ACTIONS
     action = actionData(actionIdx);
@@ -17,10 +19,11 @@ function [ClassProb PairProb] = BaumWelch(P, actionData, poseData, logEmissionPr
     ForwardLogProbs(1, :) = log(P.c) + logEmissionProbs(poseIdxs(1), :);
 
     for poseIdxIdx=2:NUM_POSES
-      % Probability of state given observation and preceeding data.
-      ForwardLogProbs(poseIdxIdx, :) = ...
-        sum(ForwardLogProbs(poseIdxIdx - 1, :)' .+ P.transMatrix) .+ ...
-        logEmissionProbs(poseIdxs(poseIdxIdx), :);
+      % Probability of state given observations up to poseIdxIdx.
+      ForwardLogProbs(poseIdxIdx, :) = logsumexp((
+        ForwardLogProbs(poseIdxIdx - 1, :)'
+        .+ logTransMatrix
+        .+ logEmissionProbs(poseIdxs(poseIdxIdx), :))')';
 
       % Normalize.
       ForwardLogProbs(poseIdxIdx, :) -= ...
@@ -30,10 +33,11 @@ function [ClassProb PairProb] = BaumWelch(P, actionData, poseData, logEmissionPr
     BackwardLogProbs = zeros(NUM_POSES, NUM_CLASSES);
     BackwardLogProbs(NUM_POSES, :) = ones(1, NUM_CLASSES);
     for poseIdxIdx=fliplr(1:(NUM_POSES-1))
-      % Probability of state given observation and following data.
-      BackwardLogProbs(poseIdxIdx, :) = ...
-        sum(BackwardLogProbs(poseIdxIdx + 1, :) .+ P.transMatrix, 2)' .+ ...
-        logEmissionProbs(poseIdxs(poseIdxIdx), :);
+      % Probability of subsequence observations given state.
+      BackwardLogProbs(poseIdxIdx, :) = logsumexp(
+        logTransMatrix .+
+        logEmissionProbs(poseIdxs(poseIdxIdx + 1), :) .+
+        BackwardLogProbs(poseIdxIdx + 1, :))';
 
       % Normalize.
       BackwardLogProbs(poseIdxIdx, :) -= ...
@@ -45,6 +49,19 @@ function [ClassProb PairProb] = BaumWelch(P, actionData, poseData, logEmissionPr
         BackwardLogProbs(poseIdxIdx, :);
       logProbs -= logsumexp(logProbs);
       ClassProb(poseIdxs(poseIdxIdx), :) = exp(logProbs);
+    end
+
+    for poseIdxIdx=1:(NUM_POSES-1)
+      logProbs = (
+        ForwardLogProbs(poseIdxIdx, :)' .+
+        logTransMatrix .+
+        logEmissionProbs(poseIdxIdx + 1, :) .+
+        BackwardLogProbs(poseIdxIdx + 1, :));
+
+      logProbs = reshape(logProbs, 1, NUM_CLASSES ^ 2);
+      logProbs -= logsumexp(logProbs);
+
+      PairProb(action.pair_ind(poseIdxIdx), :) = exp(logProbs);
     end
   end
 end
