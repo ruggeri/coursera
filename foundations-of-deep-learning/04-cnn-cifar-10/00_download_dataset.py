751910ba2cbad5bf7cd35ba32f34db4fd1b43668
Ned Ruggeri <ruggeri@self-loop.com>
2017-03-06 13:12:36 -0800

    make data load script loadable without running

--- foundations-of-deep-learning/04-cnn-cifar-10/00_download_dataset.py
+++ foundations-of-deep-learning/04-cnn-cifar-10/00_download_dataset.py
@@ -8,19 +8,31 @@ cifar10_dataset_folder_path = 'cifar-10-batches-py'
 class DLProgress(tqdm):
     last_block = 0
 
+    def __init__(self):
+        super().__init__(
+            unit="B",
+            unit_scale=True,
+            miniters=1,
+            desc="CIFAR-10 Dataset"
+        )
+
     def hook(self, block_num=1, block_size=1, total_size=None):
         self.total = total_size
         self.update((block_num - self.last_block) * block_size)
         self.last_block = block_num
 
-if not isfile('cifar-10-python.tar.gz'):
-    with DLProgress(unit='B', unit_scale=True, miniters=1, desc='CIFAR-10 Dataset') as pbar:
-        urlretrieve(
-            'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz',
-            'cifar-10-python.tar.gz',
-            pbar.hook)
+def run():
+    if not isfile('cifar-10-python.tar.gz'):
+        with DLProgress() as pbar:
+            urlretrieve(
+                'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz',
+                'cifar-10-python.tar.gz',
+                pbar.hook)
 
-if not isdir(cifar10_dataset_folder_path):
-    with tarfile.open('cifar-10-python.tar.gz') as tar:
-        tar.extractall()
-        tar.close()
+    if not isdir(cifar10_dataset_folder_path):
+        with tarfile.open('cifar-10-python.tar.gz') as tar:
+            tar.extractall()
+            tar.close()
+
+if __name__ == "__main__":
+    run()
