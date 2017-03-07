efa671267b6a9a5dd19a210c46ab4856a43a63b3
Ned Ruggeri <ruggeri@self-loop.com>
2017-03-06 19:23:06 -0800

    remove old config and pipeline files

--- driverless-car/p1-lane-lines/main.py
+++ driverless-car/p1-lane-lines/main.py
@@ -1,10 +1,13 @@
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 from moviepy.editor import VideoFileClip
+import os
 
 from lib.runner import Runner
 
 def run_image(image_path):
+    print(image_path)
+
     image = mpimg.imread(image_path)
     runner = Runner(image.shape)
     runner.run(image)
@@ -12,6 +15,11 @@ def run_image(image_path):
     plt.figure()
     plt.imshow(image)
     plt.show()
+
+def run_images():
+    for path in os.listdir("tests/jpgs/"):
+        image_path = "tests/jpgs/{}".format(path)
+        run_image(image_path)
 
 class RunnerWrapper:
     def __init__(self):
@@ -33,5 +41,5 @@ def run_movie(name):
     challenge_clip.write_videofile(output_path, audio = False)
 
 if __name__ == "__main__":
-    run_image("tests/jpgs/solidWhiteCurve.jpg")
+    run_images()
     #run_movie("challenge.mp4")
