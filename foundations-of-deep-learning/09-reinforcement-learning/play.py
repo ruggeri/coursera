9967acdbe26e47a64583ad8dc346518ff82bc32c
Ned Ruggeri <ruggeri@self-loop.com>
2017-04-23 19:53:50 -0700

    try to always play the ideal position

--- foundations-of-deep-learning/09-reinforcement-learning/play.py
+++ foundations-of-deep-learning/09-reinforcement-learning/play.py
@@ -20,6 +20,37 @@ def choose_action(session, graph, game_state):
     best_action = np.argmax(q_values)
     return best_action
 
+# This code tells us to take the locally optimal action.
+def choose_best_action(session, graph, game_state):
+    import pong_state
+
+    game = pong.PongGame(training_mode = True)
+    game.state.paddle1_pos = game_state[0]
+    game.state.paddle2_pos = game_state[1]
+    game.state.ball_pos = np.array([game_state[2], game_state[3]])
+    game.state.ball_vel = np.array([game_state[4], game_state[5]])
+    prev_state = game.state
+    prev_stats = game.stats
+    play_action_idx(game, 0)
+    next_state = game.state
+    next_stats = game.stats
+    reward0 = example.reward(prev_state, prev_stats, next_state, next_stats)
+
+    game = pong.PongGame(training_mode = True)
+    game.state.paddle1_pos = game_state[0]
+    game.state.paddle2_pos = game_state[1]
+    game.state.ball_pos = np.array([game_state[2], game_state[3]])
+    game.state.ball_vel = np.array([game_state[4], game_state[5]])
+    prev_state = game.state
+    prev_stats = game.stats
+    play_action_idx(game, 1)
+    next_state = game.state
+    next_stats = game.stats
+    reward1 = example.reward(prev_state, prev_stats, next_state, next_stats)
+
+    return 1 if reward0 < reward1 else 0
+
+
 def play_action_idx(game, chosen_action_idx):
     chosen_action_name = example.action_idx_to_action_name(
         chosen_action_idx
@@ -45,9 +76,14 @@ async def async_evaluate_performance(
     while total_points(game) < config.NUM_POINTS_PER_EVALUATION:
         prev_stats = game.stats
         prev_game_state = example.model_state(game.state)
-        chosen_action_idx = choose_action(
-            session, graph, prev_game_state
-        )
+        if config.CHOOSE_BEST_ALWAYS:
+            chosen_action_idx = choose_best_action(
+                session, graph, prev_game_state
+            )
+        else:
+            chosen_action_idx = choose_action(
+                session, graph, prev_game_state
+            )
         play_action_idx(game, chosen_action_idx)
         next_stats = game.stats
 
