fbf9e6a96749a36aa78cb0e866feb10c8fc06af2
Ned Ruggeri <ruggeri@self-loop.com>
2017-05-03 12:36:45 -0700

    seq2seq: do some basic training; seems to work very well!

--- foundations-of-deep-learning/10-seq2seq/config.py
+++ foundations-of-deep-learning/10-seq2seq/config.py
@@ -1,14 +1,10 @@
 BATCH_SIZE = 128
 EXAMPLE_MAX_LEN = 10
+LEARNING_RATE = 0.001
 # This is just for practice; it would be useful if there were many,
 # many words in the vocabulary.
 NUM_EMBEDDING_DIMENSIONS = 13
+NUM_EPOCHS = 60
 NUM_EXAMPLES = 10000
 NUM_LSTM_LAYERS = 2
 NUM_LSTM_UNITS = 64
-
-# TODO
-## Number of Epochs
-#epochs = 60
-## Learning Rate
-#learning_rate = 0.001
--- foundations-of-deep-learning/10-seq2seq/main.py
+++ foundations-of-deep-learning/10-seq2seq/main.py
@@ -1,22 +1,47 @@
 import config
 import dataset
 import graph
+import random
 import tensorflow as tf
 
-d = dataset.dataset(
-    max_length = config.EXAMPLE_MAX_LEN,
-    num_examples = config.NUM_EXAMPLES
-)
+def run_batch(session, d, g):
+    examples = random.sample(d.idx_examples, config.BATCH_SIZE)
+    _, loss = session.run(
+        [g.training_op, g.training_loss],
+        feed_dict = {
+            g.input_sequence: [e[0] for e in examples],
+            g.output_sequence: [e[1] for e in examples],
+            g.learning_rate: config.LEARNING_RATE
+        }
+    )
 
-g = graph.graph(
-    batch_size = config.BATCH_SIZE,
-    sequence_length = config.EXAMPLE_MAX_LEN,
-    vocab_size = len(d.word_to_idx),
-    num_embedding_dimensions = config.NUM_EMBEDDING_DIMENSIONS,
-    num_lstm_layers = config.NUM_LSTM_LAYERS,
-    num_lstm_units = config.NUM_LSTM_UNITS,
-    start_word_idx = d.word_to_idx[dataset.start_word],
-    stop_word_idx = d.word_to_idx[dataset.stop_word],
-)
+    print(f"Loss: {loss}")
 
-file_writer = tf.summary.FileWriter('logs/', tf.get_default_graph())
+def run(session):
+    d = dataset.dataset(
+        max_length = config.EXAMPLE_MAX_LEN,
+        num_examples = config.NUM_EXAMPLES
+    )
+
+    g = graph.graph(
+        batch_size = config.BATCH_SIZE,
+        sequence_length = config.EXAMPLE_MAX_LEN,
+        vocab_size = len(d.word_to_idx),
+        num_embedding_dimensions = config.NUM_EMBEDDING_DIMENSIONS,
+        num_lstm_layers = config.NUM_LSTM_LAYERS,
+        num_lstm_units = config.NUM_LSTM_UNITS,
+        start_word_idx = d.word_to_idx[dataset.start_word],
+        stop_word_idx = d.word_to_idx[dataset.stop_word],
+    )
+
+    session.run(tf.global_variables_initializer())
+
+    file_writer = tf.summary.FileWriter('logs/', session.graph)
+
+    for epoch_idx in range(1, config.NUM_EPOCHS):
+        num_batches = len(d.idx_examples) // config.BATCH_SIZE
+        for batch_idx in range(1, num_batches):
+            run_batch(session, d, g)
+
+with tf.Session() as session:
+    run(session)
