a41de74f30dd81abfbaa40369feb69d7c07cd76d
Ned Ruggeri <ruggeri@self-loop.com>
2017-05-14 14:40:38 -0700

    gan: review batch.py, expose class information
    I suspect we will need multiplicative connections

--- foundations-of-deep-learning/11-gan/batch.py
+++ foundations-of-deep-learning/11-gan/batch.py
@@ -14,18 +14,17 @@ Batch = namedtuple("Batch", [
 ])
 
 def generate_z(num_samples):
-    # TODO: Appears to be very important that this is uniform!!
+    # TODO: When I used a normal distribution to generate the z values
+    # the losses were much higher. I wonder if I tried again to change
+    # it back whether that would still be the case.
     return np.random.uniform(
-        -1,
-        +1,
+        low = -1,
+        high = +1,
         size = [num_samples, config.Z_DIMS]
     )
 
 def generate_x(run_info, num_samples, fake_class_label, z):
     session, graph = run_info.session, run_info.graph
-    if z is None:
-        z = generate_z(num_samples)
-
     return session.run(graph.generator.generated_x, feed_dict = {
         graph.generator.class_label: fake_class_label,
         graph.generator.z: z,
@@ -35,8 +34,6 @@ def generate_fake_data(run_info, num_classes, num_samples):
     fake_class_label = np.random.choice(
         num_classes + 1, size = num_samples, replace = True
     )
-    # TODO: Trying unconditional to get that working first...
-    fake_class_label = np.ones_like(fake_class_label)
     fake_z = generate_z(num_samples)
     fake_x = generate_x(
         run_info = run_info,
@@ -47,10 +44,8 @@ def generate_fake_data(run_info, num_classes, num_samples):
 
     return (fake_class_label, fake_z, fake_x)
 
-def generate_real_data(dataset, num_samples):
+def select_real_data(dataset, num_samples):
     real_x, real_class_label = dataset.next_batch(num_samples)
-    # TODO: Trying unconditional to get that working first...
-    real_class_label = np.ones_like(real_class_label)
 
     # Reshape and renormalize to (-1, +1)
     real_x = real_x.reshape([-1, config.IMAGE_DIMS])
@@ -64,7 +59,7 @@ def next_batch(run_info, batch_size):
         num_classes = config.NUM_CLASSES,
         num_samples = batch_size,
     )
-    real_class_label, real_x = generate_real_data(
+    real_class_label, real_x = select_real_data(
         dataset = run_info.dataset,
         num_samples = batch_size,
     )
