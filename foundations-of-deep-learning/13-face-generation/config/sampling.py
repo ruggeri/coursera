cc4b708d326bc2f7bdbadc6a9a56ee5ad60c2b10
Ned Ruggeri <ruggeri@self-loop.com>
2017-05-21 01:49:21 +0000

    face generation: fix sampling use of config.COLOR_MODE

--- foundations-of-deep-learning/13-face-generation/config/sampling.py
+++ foundations-of-deep-learning/13-face-generation/config/sampling.py
@@ -1,1 +1,10 @@
+import config
+
+if config.IMAGE_DIMS[2] == 1:
+    COLOR_MODE = "L"
+elif config.IMAGE_DIMS[2] == 3:
+    COLOR_MODE = "RGB"
+else:
+    raise Exception("Unrecognized color depth!")
+
 NUM_SAMPLES_PER_SAMPLING = 9
